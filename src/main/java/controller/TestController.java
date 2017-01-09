df7c6014a95934acfc9f317ba9480218cfc4a0ef
igorKryvenko <kryvenko7igor@gmail.com>
2017-01-09 13:12:22 +0200

    Initial commit

---
+++ src/main/java/controller/TestController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Igor on 09.01.2017.
+ */
+@Controller
+@RequestMapping("/hello")
+public class TestController {
+}
