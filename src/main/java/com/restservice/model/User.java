346fb9ee4a23c304fdfd1944734534950b10dbd7
igorKryvenko <kryvenko7igor@gmail.com>
2017-01-13 17:21:08 +0200

    Add the User class

---
+++ src/main/java/com/restservice/model/User.java
@@ -0,0 +1,21 @@
+package com.restservice.model;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+public class User implements UserDetails {
+    private List<GrantedAuthority> authorities;
+    private String password;
+    private String username;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialNonExpired;
+    private boolean enabled;
+
+}
