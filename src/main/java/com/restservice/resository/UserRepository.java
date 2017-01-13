581b0f1871b6c328a178de1261178980e139092e
igorKryvenko <kryvenko7igor@gmail.com>
2017-01-13 18:22:46 +0200

    Realize the findByUsername function

--- src/main/java/com/restservice/resository/UserRepository.java
+++ src/main/java/com/restservice/resository/UserRepository.java
@@ -10,4 +10,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface UserRepository extends MongoRepository<User,ObjectId> {
+    public User findByUsername(String username);
 }
--- src/main/java/com/restservice/service/UserService.java
+++ src/main/java/com/restservice/service/UserService.java
@@ -1,5 +1,7 @@
 package com.restservice.service;
 
+import com.restservice.resository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,10 +9,13 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 /**
  * Created by Igor on 13.01.2017.
  */
+
 public class UserService implements UserDetailsService {
 
+    @Autowired
+    private UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+        return userRepository.findByUsername(s);
     }
 }
