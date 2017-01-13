4da998ef6f7ba9d3aca7089200de39dae0d13e92
igorKryvenko <kryvenko7igor@gmail.com>
2017-01-13 17:49:12 +0200

    Modify CurrencyRepository

--- src/main/java/com/restservice/resository/CurrencyRepository.java
+++ src/main/java/com/restservice/resository/CurrencyRepository.java
@@ -3,10 +3,12 @@ package com.restservice.resository;
 import com.restservice.model.Currency;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by Igor on 13.01.2017.
  */
+@Repository
 public interface CurrencyRepository extends MongoRepository<Currency, String> {
 
 }
