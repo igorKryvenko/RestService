495ccb886d14dcc9eb927e1cfd6be399b4ca2565
igorKryvenko <kryvenko7igor@gmail.com>
2017-01-13 17:38:48 +0200

    Create UserService

--- src/main/java/com/restservice/service/UserService.java
+++ src/main/java/com/restservice/service/UserService.java
@@ -1,7 +1,16 @@
 package com.restservice.service;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 /**
  * Created by Igor on 13.01.2017.
  */
-public class UserService {
+public class UserService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
 }
