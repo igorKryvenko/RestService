fdbeb634b06728c3ccd16c4d3ee2dabedade777a
igorKryvenko <kryvenko7igor@gmail.com>
2017-01-13 17:38:27 +0200

    Modify the User class for MongoDb

--- src/main/java/com/restservice/model/User.java
+++ src/main/java/com/restservice/model/User.java
@@ -2,6 +2,10 @@ package com.restservice.model;
 
 
 import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,10 +13,13 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import java.util.Collection;
 import java.util.List;
 
-
+@Document(collection = "users")
 public class User implements UserDetails{
+    @Id
+    private ObjectId objectId;
     private List<Role> authorities;
     private String password;
+    @Indexed(unique = true)
     private String username;
     private boolean accountNonExpired;
     private boolean accountNonLocked;
---
+++ src/main/java/com/restservice/service/UserService.java
@@ -0,0 +1,7 @@
+package com.restservice.service;
+
+/**
+ * Created by Igor on 13.01.2017.
+ */
+public class UserService {
+}
