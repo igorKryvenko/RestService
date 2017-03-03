2222702ff15eb36fb82e2288b04816f67605e3dd
igorKryvenko <kryvenko7igor@gmail.com>
2017-03-03 03:38:26 +0200

    Rewrite the Role class

--- src/main/java/com/restservice/model/Role.java
+++ src/main/java/com/restservice/model/Role.java
@@ -1,15 +1,53 @@
 package com.restservice.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
+
+import java.util.List;
 
 /**
  * Created by Igor on 13.01.2017.
  */
-public enum Role implements GrantedAuthority {
-    USER;
+@Document(collection="roles")
+public class Role  {
+    @Id
+    private Long id;
 
-    @Override
-    public String getAuthority() {
-        return name();
+    @DBRef(lazy = true)
+    private List<User> users;
+
+    private String name;
+
+    public Role(){
+
+    }
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
