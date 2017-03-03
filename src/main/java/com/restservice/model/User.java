b7169d5427d66857d51fdd6a1f00d89db06c8126
igorKryvenko <kryvenko7igor@gmail.com>
2017-03-03 03:28:05 +0200

    Rewrite the User class

--- src/main/java/com/restservice/model/User.java
+++ src/main/java/com/restservice/model/User.java
@@ -18,99 +18,71 @@ import java.util.List;
 
 @Document(collection = "users")
 @Data
-
 @AllArgsConstructor
 @Builder
-public class User implements UserDetails{
+public class User {
     @Id
     private ObjectId objectId;
     private List<Role> authorities;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Indexed
+    private String email;
+
     private String password;
-    @Indexed(unique = true)
-    private String username;
-    private boolean accountNonExpired;
-    private boolean accountNonLocked;
-    private boolean credentialsNonExpired;
-    private boolean enabled;
+
 
     public User() {
     }
 
-    public User( List<Role> authorities, String password, String username, boolean accountNonExpired, boolean accountNonLocked, boolean credentialsNonExpired, boolean enabled) {
-
-        this.authorities = authorities;
-        this.password = password;
-        this.username = username;
-        this.accountNonExpired = accountNonExpired;
-        this.accountNonLocked = accountNonLocked;
-        this.credentialsNonExpired = credentialsNonExpired;
-        this.enabled = enabled;
-    }
-
-    @Override
-    public List<Role> getAuthorities() {
-        return authorities;
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return username;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return accountNonExpired;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return accountNonLocked;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return credentialsNonExpired;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return enabled;
+    public ObjectId getObjectId() {
+        return objectId;
     }
 
     public void setObjectId(ObjectId objectId) {
         this.objectId = objectId;
     }
 
+    public List<Role> getAuthorities() {
+        return authorities;
+    }
+
     public void setAuthorities(List<Role> authorities) {
         this.authorities = authorities;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setAccountNonExpired(boolean accountNonExpired) {
-        this.accountNonExpired = accountNonExpired;
-    }
-
-    public void setAccountNonLocked(boolean accountNonLocked) {
-        this.accountNonLocked = accountNonLocked;
-    }
-
-    public void setCredentialsNonExpired(boolean credentialsNonExpired) {
-        this.credentialsNonExpired = credentialsNonExpired;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
     }
 }
