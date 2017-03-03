560c69fcc3c864a042d178dd1c52d8201b8d0fa2
igorKryvenko <kryvenko7igor@gmail.com>
2017-03-03 03:41:16 +0200

    Rewrite UserService class

--- src/main/java/com/restservice/service/UserService.java
+++ src/main/java/com/restservice/service/UserService.java
@@ -5,27 +5,44 @@ import com.restservice.model.Role;
 import com.restservice.model.User;
 import com.restservice.resository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Igor on 13.01.2017.
  */
 @Service
 public class UserService implements UserDetailsService {
-
-
-
     @Autowired
     private UserRepository userRepository;
 
-
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return userRepository.findByUsername(s);
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(email);
+        if(user == null) {
+            throw new UsernameNotFoundException("No users found with username " + email);
+        }
+        return  new org.springframework.security.core.userdetails.User
+                (user.getEmail(),
+                        user.getPassword().toLowerCase(), true, true,
+                        true, true,
+                        getAuthorities(user.getAuthorities()));
+    }
+
+    private static List<GrantedAuthority> getAuthorities (List<Role> roles) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for (Role  role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        return authorities;
     }
 }
