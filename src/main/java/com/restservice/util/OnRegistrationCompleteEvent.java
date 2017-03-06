877998b0fb7da5cfa5a4fb9f318972b80b7a19a9
igorKryvenko <kryvenko7igor@gmail.com>
2017-03-06 20:40:37 +0200

    Realize  OnRegistrationCompleteEvent class

--- src/main/java/com/restservice/util/OnRegistrationCompleteEvent.java
+++ src/main/java/com/restservice/util/OnRegistrationCompleteEvent.java
@@ -1,7 +1,36 @@
 package com.restservice.util;
 
+import com.restservice.model.User;
+import org.springframework.context.ApplicationEvent;
+
+import java.util.Locale;
+
 /**
  * Created by Igor on 06.03.2017.
  */
-public class OnRegistrationCompleteEvent {
+public class OnRegistrationCompleteEvent extends ApplicationEvent {
+    private final String appUrl;
+    private final Locale locale;
+    private final User user;
+
+    public OnRegistrationCompleteEvent(final User user, final Locale locale, final String appUrl) {
+        super(user);
+        this.user = user;
+        this.locale = locale;
+        this.appUrl = appUrl;
+    }
+
+
+
+    public String getAppUrl() {
+        return appUrl;
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public User getUser() {
+        return user;
+    }
 }
