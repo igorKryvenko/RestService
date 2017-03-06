985bc3abfc03598232a7c2fa2b8debfdcb573942
igorKryvenko <kryvenko7igor@gmail.com>
2017-03-06 20:42:43 +0200

    Realize the registerAccount method of RegistrationController class

--- src/main/java/com/restservice/controller/RegistrationController.java
+++ src/main/java/com/restservice/controller/RegistrationController.java
@@ -1,7 +1,49 @@
 package com.restservice.controller;
 
+
+import com.restservice.dto.UserDto;
+import com.restservice.model.User;
+import com.restservice.service.UserService;
+import com.restservice.util.GenericResponse;
+import com.restservice.util.OnRegistrationCompleteEvent;
+import org.hibernate.annotations.common.util.impl.LoggerFactory;
+
+import org.slf4j.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+
 /**
  * Created by Igor on 06.03.2017.
  */
-public class RegistrationControllerq {
+@Controller
+public class RegistrationController {
+    private final Logger LOGGER = (Logger) LoggerFactory.logger(getClass());
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
+    @RequestMapping(value = "/user/registration" , method = RequestMethod.POST)
+    @ResponseBody
+    public GenericResponse registerAccount(final UserDto userDto,HttpServletRequest request) {
+        LOGGER.debug("Registartion account with data: ",userDto);
+
+        User registered = userService.registerNewUser(userDto);
+        publisher.publishEvent(new OnRegistrationCompleteEvent(registered,request.getLocale(),getAppUrl(request)));
+        return new GenericResponse("success");
+    }
+
+    private String getAppUrl(HttpServletRequest request) {
+        return "http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
+    }
 }
